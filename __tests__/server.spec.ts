d5301133c89f6e53eb32fa09ed44c08f67d3ab1a
BlackGlory <woshenmedoubuzhidao@blackglory.me>
2021-11-19 09:04:39 +0000

    refactor: remove applyRequest

--- __tests__/server.spec.ts
+++ __tests__/server.spec.ts
@@ -1,23 +1,16 @@
 import { createResponse } from '@src/server'
 import '@blackglory/jest-matchers'
 
-interface IAPI {
-  echo(message: string): Promise<string>
-  throws(): Promise<void>
-}
-
-const api: IAPI = {
-  async echo(message: string): Promise<string> {
-    return message
-  }
-, async throws() {
-    throw new Error('message')
-  }
-}
-
-describe('createResponse', () => {
+describe(`
+  createResponse<Obj extends object, DataType = unknown>(
+    api: Obj
+  , request: IRequest<DataType>
+  ): Promise<IResponse<DataType>>
+`, () => {
   describe('method returns result', () => {
     it('return IResult', async () => {
+      const method = jest.fn().mockImplementation(async (message: string) => message)
+      const api = { echo: method }
       const request: IRequest<unknown> = {
         protocol: 'delight-rpc'
       , version: '1.0'
@@ -37,10 +30,37 @@ describe('createResponse', () => {
       , result: 'message'
       })
     })
+
+    test('method with namespace', async () => {
+      const method = jest.fn().mockImplementation(async (message: string) => message)
+      const api = {
+        namespace: { echo: method }
+      }
+      const request: IRequest<unknown> = {
+        protocol: 'delight-rpc'
+      , version: '1.0'
+      , id: 'id'
+      , method: ['namespace', 'echo']
+      , params: ['message']
+      }
+
+      const result = createResponse(api, request)
+      const proResult = await result
+
+      expect(result).toBePromise()
+      expect(method).toBeCalledTimes(1)
+      expect(proResult).toStrictEqual({
+        protocol: 'delight-rpc'
+      , version: '1.0'
+      , id: 'id'
+      , result: 'message'
+      })
+    })
   })
 
   describe('method not available', () => {
     it('return IError', async () => {
+      const api = {}
       const request: IRequest<unknown> = {
         protocol: 'delight-rpc'
       , version: '1.0'
@@ -67,6 +87,10 @@ describe('createResponse', () => {
 
   describe('method throws error', () => {
     it('return IError', async () => {
+      const method = jest.fn().mockImplementation(async () => {
+        throw new Error('message')
+      })
+      const api = { throws: method }
       const request: IRequest<unknown> = {
         protocol: 'delight-rpc'
       , version: '1.0'
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,31 @@
-import { applyRequest } from '@utils/apply-request'
+import { isntFunction, isError } from '@blackglory/types'
+import { createResult } from '@utils/create-result'
+import { createError } from '@utils/create-error'
+import { tryGetProp } from 'object-path-operator'
+import { assert } from '@blackglory/errors'
 
 export async function createResponse<Obj extends object, DataType = unknown>(
   api: Obj
 , request: IRequest<DataType>
 ): Promise<IResponse<DataType>> {
-  return await applyRequest(api, request)
+  try {
+    const fn = tryGetProp(api, request.method)
+    if (isntFunction(fn)) {
+      return createError(
+        request.id
+      , 'MethodNotAvailable'
+      , 'The method is not available.'
+      )
+    }
+
+    try {
+      const result = await Reflect.apply(fn, api, request.params)
+      return createResult(request.id, result)
+    } catch (e) {
+      assert(isError(e), 'The thrown object must be an Error')
+      return createError(request.id, e.name, e.message)
+    }
+  } catch (e) {
+    return createError(request.id, 'InternalError', `${e}`)
+  }
 }
